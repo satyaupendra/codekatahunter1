fa113618335a66aab41f53a2f82f06f0f9ef119a
Satya Upendra <36037076+satyaupendra@users.noreply.github.com>
2019-05-13 00:30:09 +0530

    Update codekatahunter1.py

--- codekatahunter1.py
+++ codekatahunter1.py
@@ -1,9 +1,13 @@
 n=int(input())
 r=[]
+t=0
 l=list(map(int,input().split()))
 for i in range(n):
     for j in range(i+1,n):
         if((l[i]==l[j]) and l[i] not in r):
             r.append(l[i])
+            t=1
 r.sort()
 print(" ".join(map(str,r)))
+if(t==0):
+    print('unique')
