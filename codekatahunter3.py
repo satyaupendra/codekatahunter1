3e146b7cf7cd64d43bc098c73a165fbb72f07824
Satya Upendra <36037076+satyaupendra@users.noreply.github.com>
2019-05-13 15:58:24 +0530

    Update codekatahunter3.py

--- codekatahunter3.py
+++ codekatahunter3.py
@@ -1,11 +1,11 @@
 n=int(input())
-r=[]
+s=[]
 l=list(map(int,input().split()))
 for i in range(n):
     if(i==l[i]):
-        r.append(l[i])
-r.sort()
-if(len(r)==0):
+        s.append(l[i])
+s.sort()
+if(len(s)==0):
     print("-1")
 else:
-    print(" ".join(map(str,r)))
+    print(" ".join(map(str,s)))
