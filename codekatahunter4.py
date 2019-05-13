81d37454c0933c40cff889338b32b2e38d7f20d2
Satya Upendra <36037076+satyaupendra@users.noreply.github.com>
2019-05-13 16:02:27 +0530

    Update codekatahunter4.py

--- codekatahunter4.py
+++ codekatahunter4.py
@@ -1,5 +1,5 @@
 n=int(input())
-l=list(map(int,input().split()))
+k=list(map(int,input().split()))
 for i in range(n):
-    if(l.count(l[i])==1):
-        print(l[i])
+    if(k.count(k[i])==1):
+        print(k[i])
