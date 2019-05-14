ec8744ab7258f7878d78e569a3eef3bfe615de1c
Satya Upendra <36037076+satyaupendra@users.noreply.github.com>
2019-05-14 20:03:21 +0530

    Update codekatahunter7.py

--- codekatahunter7.py
+++ codekatahunter7.py
@@ -1,9 +1,9 @@
-n=int(input())
+n1=int(input())
 l=list(map(int,input().split()))
 even=[]
 odd=[]
 m=[]
-for i in range(n):
+for i in range(n1):
     if(l[i]%2==0 and i%2!=0):
         print(l[i],end=" ")
     elif(l[i]%2!=0 and i%2==0):
