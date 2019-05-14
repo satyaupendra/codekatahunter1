7145ae4738a693a83ae164af30ff8fd4309485d7
Satya Upendra <36037076+satyaupendra@users.noreply.github.com>
2019-05-14 21:32:06 +0530

    Update ckh126.py

--- ckh126.py
+++ ckh126.py
@@ -1,11 +1,8 @@
 n=input()
-for i in range(len(n)):
-    t=0
-    if(n[i]==" "):
-        if(n[i+1]==n[i+1].upper()):
-            t=1
-if(t==1):
-    print('yes')
+t=n.title()
+if t==n:
+    print("yes")
 else:
-    print('no')
+    print("no")
+        
         
